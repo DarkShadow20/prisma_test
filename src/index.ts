6eed0b38092309597c2fa9f4919b44dfa8d9001f
DarkShadow20 <kunal8011@gmail.com>
2024-04-05 15:01:46 +0530

    initialized prisma project

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+interface UserUpdateProps{
+    firstName:string,
+    lastName:string
+}
+
+async function insertUser(email: string, password: string, firstName: string, lastName: string) {
+  const res = await prisma.user.create({
+    data:{
+        email,
+        password,
+        firstName,
+        lastName
+    }
+  })
+  console.log(res)
+}
+
+async function updateUser(email:string,{firstName,lastName}:UserUpdateProps){
+    const res = await prisma.user.update({
+        where:{email:email},
+        data:{
+            firstName,
+            lastName
+        }
+    })
+    console.log(res)
+}
+//insertUser("kunal@gmail.com","kunal@123","Kunal","Gupta")
+// updateUser('kunal@gmail.com',{
+//     firstName:"KunalOG",
+//     lastName:"Guptaji"
+// })
+
+async function getUser(email:string){
+    const res = await prisma.user.findUnique({
+        where:{
+            email:email
+        }
+    }) 
+
+    console.log(res)
+}
+
+getUser("kunal@gmail.com")
